c305d783cb857dd8a058ca6cf0b29e9979031bfc
Jalo Hiltunen <j4l0.hiltu939@gmail.com>
2025-10-27 12:59:34 +0200

    yddysh
    yddysh

---
+++ 3. Javascript/1. Variables and Strings/2. Build a javascript trivia bot/script.js
@@ -0,0 +1,16 @@
+console.log("Hello! I'm your coding fun fact guide!");
+let botName = "ekke";
+let botLocation = "Helsinki";
+let favoriteLanguage = "golang";
+console.log(`My name is ${botName} and I live on ${botLocation}.`);
+console.log(`My favorite programming language is ${favoriteLanguage}.`);
+let codingFact = "i am gay "+favoriteLanguage;
+console.log(codingFact);
+codingFact = "i am boy "+favoriteLanguage;
+console.log(codingFact);
+codingFact = "i am trans "+favoriteLanguage;
+console.log(codingFact);
+console.log(`It was fun sharing these facts with you. Goodbye! - ${botName} from ${botLocation}.`);
+
+
+
---
+++ 3. Javascript/1. Variables and Strings/3. Sentence maker/script.js
@@ -0,0 +1,20 @@
+let adjective = "kaunis";
+let noun = "boo";
+let verb = "leipoa";
+let place = "Huvila";
+let adjective2 = "ruma";
+let noun2 = "Hullu";
+
+let firstStory = `Once upon a time, there was a(n) ${adjective} ${noun} who loved to eat ${noun2}. The ${noun} lived in a ${place} and had ${adjective2} nostrils that blew fire when it was ${verb}.`;
+console.log("First story: "+firstStory);
+
+
+adjective = "kekkosmainen";
+noun = "wanted";
+verb = "swear";
+place = "stark tower";
+noun2 = "shuuush";
+adjective2 = "crazy";
+
+let secondStory = `Once upon a time, there was a(n) ${adjective} ${noun} who loved to eat ${noun2}. The ${noun} lived in a ${place} and had ${adjective2} nostrils that blew fire when it was ${verb}.`;
+console.log("Second story: "+secondStory);
