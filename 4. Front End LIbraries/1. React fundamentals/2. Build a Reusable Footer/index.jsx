9ea0300e2f8ee25cfe58275830d1aada567fe285
Jalo Hiltunen <j4l0.hiltu939@gmail.com>
2025-11-09 12:44:04 +0200

    dd
    dd

---
+++ 4. Front End LIbraries/1. React fundamentals/2. Build a Reusable Footer/index.jsx
@@ -0,0 +1,23 @@
+export const Footer = () => {
+  return (
+    <footer>
+      <ul>
+        <li>Linkedin</li>
+        <li>Spotify</li>
+        <li>Megabricks</li>
+      </ul>
+      <ul>
+        <li>Something</li>
+        <li>Youtube</li>
+        <li>Soundcloud</li>
+      </ul>
+      <p>© Something.org 2025. All rights reserved.</p>
+      <ul>
+        <li><a href="#">🎮</a></li>
+        <li><a href="#">🐦</a></li>
+        <li><a href="#">💻</a></li>
+        <li><a href="#">🏀</a></li>
+      </ul>
+    </footer>
+  )
+};
